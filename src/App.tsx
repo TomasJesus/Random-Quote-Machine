f07d96102d328e2936a94dbd7614fe770df78975
TomásJesus <tomasjesus1904@gmail.com>
2023-10-18 18:43:03 +0100

    added quote to app

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,19 @@
+import { useState } from 'react';
 import styles from './App.module.scss';
+import QuoteComponent from './components/Quote';
 import quotes from './quotes.json';
 import { Quotes } from './types';
 
 function App() {
     const allQuotes = quotes as Quotes;
 
-    return <div></div>;
+    const [currentQuoteIndex, setCurrentQuoteIndex] = useState(0);
+
+    return (
+        <div>
+            <QuoteComponent quote={allQuotes.quotes[currentQuoteIndex]} />
+        </div>
+    );
 }
 
 export default App;
